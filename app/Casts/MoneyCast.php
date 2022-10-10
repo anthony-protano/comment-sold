25b69927f2d3bc47486338430744e7df186091e7
Anthony Protano <tonypro999@gmail.com>
2022-10-10 09:30:46 -0400

    Feature: Added money cast

---
+++ app/Casts/MoneyCast.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+
+class MoneyCast implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return string
+     */
+    public function get($model, string $key, $value, array $attributes): string
+    {
+        return '$' . number_format($value / 100, 2);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return string
+     */
+    public function set($model, string $key, $value, array $attributes): string
+    {
+        return strval($value * 100);
+    }
+}
