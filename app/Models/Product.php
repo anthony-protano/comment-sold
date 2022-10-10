0f3d3c7dd081205732189a489bc49e8f140401aa
Anthony Protano <tonypro999@gmail.com>
2022-10-10 09:32:49 -0400

    Refactor: Removed human_shipping_cost

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,8 +11,6 @@ class Product extends Model
 
     protected $guarded = ['id'];
 
-    protected $appends = ['human_shipping_price'];
-
     public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class);
@@ -21,10 +19,5 @@ class Product extends Model
     public function inventory()
     {
         return $this->hasMany(Inventory::class);
-    }
-
-    public function getHumanShippingPriceAttribute()
-    {
-        return (float) number_format(($this->shipping_price/100), 2, '.', '');
     }
 }
