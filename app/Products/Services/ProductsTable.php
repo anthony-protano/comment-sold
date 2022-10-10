461117d504132c052e78f181a4333be0f795f004
Anthony Protano <tonypro999@gmail.com>
2022-10-10 08:45:55 -0400

    Fix: Added sorting

--- app/Products/Services/ProductsTable.php
+++ app/Products/Services/ProductsTable.php
@@ -20,17 +20,16 @@ class ProductsTable extends AbstractTenantService
             ->searchInput('product_name')
             ->defaultSort('product_name')
             ->column(key: 'product_name', searchable: true)
-            ->column(key: 'style')
-            ->column(key: 'brand')
+            ->column(key: 'style', sortable: true)
+            ->column(key: 'brand', sortable: true)
             ->column(label: 'skus');
-
     }
 
     public function query(Request $request): \Illuminate\Contracts\Pagination\LengthAwarePaginator
     {
         return QueryBuilder::for($this->user->product(), $request)
             ->defaultSort('product_name')
-            ->allowedSorts(['product_name', 'style', 'brand'])
+            ->allowedSorts(['style', 'brand'])
             ->allowedFilters(['product_name'])
             ->with('inventory')
             ->paginate($request->input('perPage'))
